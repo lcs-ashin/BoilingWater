341447214047b95f93120c1641a9024273b5696b
lcs-ashin <ashin@lcs.on.ca>
2022-02-16 11:38:12 -0500

    Added a slider to change the value of celsius given from the user

--- BoilingWater/ContentView.swift
+++ BoilingWater/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: Stored Properties
+    @State var temperatureGiven = 0.0
+    
+    // MARK: Computed Properties
+    var atmosphericPressure: Double {
+        return 5 * temperatureGiven - 100
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            // Celsius
+            HStack {
+                Text("Celsius")
+                    .font(.title2.bold())
+                
+                Spacer()
+            }
+            Slider(value: $temperatureGiven,
+                   in: 80.0...200.0,
+                   step: 0.1,
+                   label: {
+                Text("Celsius")
+            },
+                   minimumValueLabel: {
+                Text("80")
+            },
+                   maximumValueLabel: {
+                Text("200")
+            })
+        }
+        .padding()
     }
 }
 
