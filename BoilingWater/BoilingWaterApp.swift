3fb0560e408512dcb84cc18a008501d3bd396906
lcs-ashin <ashin@lcs.on.ca>
2022-02-16 11:23:13 -0500

    Initial Commit

---
+++ BoilingWater/BoilingWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilingWaterApp.swift
+//  BoilingWater
+//
+//  Created by Eunbi Shin on 2022-02-16.
+//
+
+import SwiftUI
+
+@main
+struct BoilingWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
